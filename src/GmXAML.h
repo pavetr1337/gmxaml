2c6a906659dda1b14827a0aa27c3fb50f5f49654
pavet <pavet@PAVETR>
2025-07-15 18:01:59 +0300

    Добавьте файлы проекта.

---
+++ src/GmXAML.h
@@ -0,0 +1,12 @@
+﻿#pragma once
+
+#include <iostream>
+#include "../deps/pugixml/src/pugixml.hpp"
+#include <fstream>
+#include <format>
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <regex>
+#include <sstream>
+#include <filesystem>
